12d0edac2a9e1daed2e87b6c9138c1c753b04507
Freund, David <David.Freund@senacor.com>
2021-12-10 10:01:16 +0100

    Solution Day 10 - 1&2

--- 2021/10/day10.rb
+++ 2021/10/day10.rb
@@ -1,26 +1,61 @@
 require '../commons.rb'
 require 'pp'
-puts SP1
 
+lines = File.readlines(FILE).map{|x| x.strip.chars}
 
-input = File.readlines(FILE).map{|x| x.strip.chars}
-
-opening = Array.new()
-closing = Array.new()
+## SETUP
+opening = ["(", "[", "{", "<"]
+closing = [")", "]", "}", ">"]
+errorPoints,autocompletePoints  = Hash.new()
+errorPoints = { ")" => 3, "]" => 57, "}" => 1197, ">" => 25137 }
+#autocompletePoints = Hash.new()
+autocompletePoints = { ")" => 1, "]" => 2, "}" => 3, ">" => 4 }
 
 pairs = Hash.new()
+for i in 0...opening.size do
+    pairs[opening[i]] = closing[i]
+end
 
-input.each do
-    
-    c = row.pop 
-    if opening.includes? c
-        expectedClosing = pairs[c]
-    elsif c == expectedClosing.pop
-        puts "is closed agian"
-    else
-        puts "Exception found in line XY"
+pp pairs
+puts SP1
+errorScore = 0
+autocompleteScores = Array.new
+for i in 0...lines.size do
+    #puts "Analyzing line \n#{lines[i]}\n"
+    exception = false
+    expectedClosing = Array.new()
+    lines[i].each do |c|
+        
+        if opening.include? c
+        #    puts "Found #{c} - add to expectedClosing #{pairs[c]}"
+            expectedClosing << pairs[c]
+        #    puts "#{expectedClosing}"
+        elsif expectedClosing.size >0 && c == expectedClosing.last
+            expectedClosing.pop
+        #    puts "is closed agian"
+        else
+            puts "Exception found in line #{i+1}, expected #{expectedClosing.last}, found #{c}"
+            puts "Error score #{errorPoints[c]}"
+            errorScore += errorPoints[c]
+            exception = true
+            break
+        end
     end
-    if row.empty and expectedClosing.size > 0
-        puts "row is incomplete"
+    if expectedClosing.size > 0 && !exception
+        score = 0
+        expectedClosing.reverse.each do |c|
+        #    puts "Current score #{score}, multiplied #{score*5} - add #{autocompletePoints[c]}"
+            score = score*5 + autocompletePoints[c]
+        end
+        autocompleteScores << score
+    elsif !exception
+        puts "Line #{i+1} is complete"
     end
 end
+
+puts "--------"
+puts "Result: #{errorScore}"
+
+
+puts SP2
+puts "Result: #{autocompleteScores.sort[autocompleteScores.size/2]}"
