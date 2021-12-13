7779d46c2ed79f74803c5ea2090c389c5601d739
Freund, David <David.Freund@senacor.com>
2021-12-13 10:50:57 +0100

    shortened

--- 2021/13/day13.rb
+++ 2021/13/day13.rb
@@ -17,32 +17,30 @@ def import
     end
 end
 
-def foldLeft lineAt
-    puts "Folding left at #{lineAt}" if TEST
-    $dots.each do |d|
-        if d[:x]>lineAt
-            d[:x] = d[:x] - (d[:x]-lineAt)*2
-        end
-    end
-end
-
-def foldUp lineAt
-    puts "Folding up at #{lineAt}" if TEST
-    $dots.each do |d|
-        if d[:y]>lineAt
-            d[:y] = d[:y] - (d[:y]-lineAt)*2
-        end
-    end
-end
-
+    
 def fold count
     $foldingInstructions[0...count].each do |fold|
-        if fold[:direction] == "x"
-            foldLeft fold[:coordinate]
-        else
-            foldUp fold[:coordinate]
+        $dots.each do |d|
+            
+            if fold[:direction] == "x" && d[:x]>fold[:coordinate]
+                d[:x] = d[:x] - (d[:x]-fold[:coordinate])*2
+            elsif fold[:direction] == "y" && d[:y]>fold[:coordinate]
+                d[:y] = d[:y] - (d[:y]-fold[:coordinate])*2
+            end
         end
     end
+end
+
+def printResult 
+
+
+    for y in 0...YMAX do
+        line =""
+        for x in 0...XMAX do
+            line += $dots.select{|d| d[:x] == x && d[:y] == y}.size >0 ? "X" : " "
+        end
+        puts line
+    end 
 end
 
 puts SP1
@@ -53,17 +51,11 @@ dotCount = $dots.group_by{|row| [row[:x],row[:y]]}.keys.size
 puts "------"
 puts "Result: #{dotCount}"
 
-puts SP2
+puts SP2+"\n\n"
 import
 fold $foldingInstructions.size
 XMAX = $dots.map{|d| d[:x]}.max+2
 YMAX = $dots.map{|d| d[:y]}.max+2
-puts "\n\n"
-for y in 0...YMAX do
-    line =""
-    for x in 0...XMAX do
-        line += $dots.select{|d| d[:x] == x && d[:y] == y}.size >0 ? "X" : " "
-    end
-    puts line
-end
+printResult
+
 
